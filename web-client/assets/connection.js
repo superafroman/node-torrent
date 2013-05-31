66c3931a7b0704f11a37de7b8a1281d0aff0910a
david yahalomi <davidyaha@gmail.com>
2013-05-31 13:16:52 +0300

    Torrent List now refeshes

--- web-client/assets/connection.js
+++ web-client/assets/connection.js
@@ -1,14 +1,6 @@
 angular.module('connection', ['ngResource']).
     factory('Torrents', function ($resource) {
-        var Torrents = $resource('/torrentList', {
-                update: { method: 'PUT' }
-            }
-        );
-
-//        Torrents.prototype.update = function(cb) {
-//            return Torrents.update({id: this._id.$oid},
-//                angular.extend({}, this, {_id:undefined}), cb);
-//        };
+        var Torrents = $resource('/torrentList', {});
 
         return Torrents;
     });
