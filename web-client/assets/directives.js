17af771bf83d214f56de2053cc9a27512c99523a
david yahalomi <davidyaha@gmail.com>
2013-05-31 13:16:20 +0300

    Added mobile support (in progress)

--- web-client/assets/directives.js
+++ web-client/assets/directives.js
@@ -1,6 +1,68 @@
 /**
- * ----  ----
+ * ---- directives.js ----
  * User: David Yahalomi
  * Date: 21/05/13
  * Time: 23:08
  */
+
+angular.module("template/popover/popover-html-popup.html", []).run(["$templateCache", function ($templateCache) {
+    $templateCache.put("template/popover/popover-html-popup.html",
+        "<div class=\"popover {{placement}}\" ng-class=\"{ in: isOpen(), fade: animation() }\">\n" +
+            "  <div class=\"arrow\"></div>\n" +
+            "\n" +
+            "  <div class=\"popover-inner\">\n" +
+            "      <div class=\"popover-content\" ng-bind-html-unsafe=\"content\"></div>\n" +
+            "  </div>\n" +
+            "</div>\n" +
+            "");
+}]);
+
+angular.module("template/error/notfound.html", []).run(["$templateCache", function ($templateCache) {
+    $templateCache.put("template/error/notfound.html",
+        "<div>\nCould not found requested page.\n</div>\n");
+}]);
+
+angular.module('ui-directives',
+        // dependencies
+        [ 'ui.bootstrap.tooltip',
+            'template/popover/popover-html-popup.html',
+            'template/error/notfound.html' ])
+    .directive('popoverHtmlPopup', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: { content: '@', placement: '@', animation: '&', isOpen: '&' },
+            templateUrl: 'template/popover/popover-html-popup.html',
+            controller: 'GetTemplateCtrl',
+            link: function (scope, element, attrs, getTemplateCtrl) {
+                getTemplateCtrl.getTemplateByURL(attrs.content, function (data, status) {
+                    if (status == 200){
+                        scope.content = data;
+                    } else {
+                        scope.content = getTemplateCtrl.error();
+                    }
+                });
+            }
+        };
+    })
+    .directive('popoverHtml', ['$tooltip', function ($tooltip) {
+        return $tooltip('popoverHtml', 'popoverHtml', 'click');
+    }]);
+
+function GetTemplateCtrl($log, $http, $templateCache) {
+    this.error = function () {
+        return $templateCache.get("template/error/notfound.html");
+    };
+
+    this.getTemplateByURL = function (url, finishCb) {
+        $log.info(url);
+        url = 'add.html';
+        if (url) {
+            $http({method: 'GET', url: url, cache: true}).
+                success(finishCb).
+                error(finishCb);
+        } else {
+            finishCb(this.error());
+        }
+    }
+}
--- web-client/assets/web-client.js
+++ web-client/assets/web-client.js
@@ -1,27 +1,33 @@
-angular.module('web-client', ['connection']).
+var app = angular.module('web-client', ['connection', 'byte-filters', 'ui.bootstrap', 'ui-directives']).
     config(function ($routeProvider) {
         $routeProvider.
             when('/', {controller: ListCtrl, templateUrl: 'list.html'}).
-            when('/new', {controller: CreateCtrl, templateUrl: 'add.html'}).
             otherwise({redirectTo: '/'});
     });
 
-function ListCtrl($scope, Torrents) {
-    $scope.torrents = Torrents.query();
-}
-
-function CreateCtrl($scope, $location, Torrents) {
-    var urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
-    var magnetPattern = /magnet:\?xt=urn:\S{20,200}/i;
-
-    $scope.save = function () {
-        if ($scope.torrent.url &&
-            ((magnetPattern.test($scope.torrent.url)) ||
-                urlPattern.test($scope.torrent.url))) {
-
-            Torrents.save($scope.torrent, function () {
-                $location.path('/');
-            });
-        }
+function ListCtrl($scope, $timeout, Torrents) {
+    function timedQuery() {
+        $timeout(function() {
+            $scope.torrents = Torrents.query();
+            timedQuery();
+        },($scope.torrents && $scope.torrents.length > 0 ? 1000 : 5000));
     }
+
+    timedQuery();
 }
+
+//app.run('$scope', '$location', function($scope, $location, Torrents) {
+//    var urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
+//    var magnetPattern = /magnet:\?xt=urn:\S{20,200}/i;
+//
+//    $scope.save = function () {
+//        if ($scope.torrent.url &&
+//            ((magnetPattern.test($scope.torrent.url)) ||
+//                urlPattern.test($scope.torrent.url))) {
+//
+//            Torrents.save($scope.torrent, function () {
+//                $location.path('/');
+//            });
+//        }
+//    }
+//});
