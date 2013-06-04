6604d5006ff27de7055d935c9c4ef999f6898e63
david yahalomi <davidyaha@gmail.com>
2013-06-04 16:15:16 +0300

    Changed the add popover to work with angular-strap rather than ui-bootstrap because of the built in html reference and hide feature. Threw out everything that wasn't necessary anymore.

--- web-client/assets/js/directives.js
+++ web-client/assets/js/directives.js
@@ -4,44 +4,5 @@
  * Date: 21/05/13
  * Time: 23:08
  */
-angular.module('ui-directives',
-        // dependencies
-        [ 'ui.bootstrap.tooltip'])
-    .directive('popoverHtmlPopup', function () {
-        return {
-            restrict: 'E',
-            replace: true,
-            scope: { content: '@', placement: '@', animation: '&', isOpen: '&' },
-            templateUrl: 'partials/directives/popover/popover-html-popup.html',
-            controller: 'GetTemplateCtrl',
-            link: function postLink(scope, element, attrs, getTemplateCtrl) {
-                // TODO The tt_content refernce is a hack.. need to find a better way of getting content value.
-                getTemplateCtrl.getTemplateByURL(scope.$parent.tt_content, function (data, status) {
-                    if (status == 200) {
-                        scope.content = data;
-                    } else {
-                        scope.content = getTemplateCtrl.error();
-                    }
-                });
-            }
-        };
-    })
-    .directive('popoverHtml', ['$tooltip', function ($tooltip) {
-        return $tooltip('popoverHtml', 'popoverHtml', 'click');
-    }]);
 
-function GetTemplateCtrl($log, $http, $templateCache) {
-    this.error = function () {
-        return $templateCache.get("partials/error/notfound.html");
-    };
-
-    this.getTemplateByURL = function (url, finishCb) {
-        if (url) {
-            $http({method: 'GET', url: url, cache: true}).
-                success(finishCb).
-                error(finishCb);
-        } else {
-            finishCb(this.error());
-        }
-    }
-}
+// At the moment, there is no need for former implementations of popover directive so the file was cleaned.
